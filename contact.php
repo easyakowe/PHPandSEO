371e13b40d4eb11ff5e6e82d3c4b13718f3556e1
Israel <easyakowe@gmail.com>
2019-09-08 17:38:52 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>	
+<meta charset="UTF-8">
+
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Primary Meta Tags -->
+    <title>Akowe Israel | Android/ Web Backend Developer | Computer Scientist</title>
+    <meta name="title" content="Akowe Israel | Android/Backend Web Developer | Machine Learning Engr. | Computer Scientist">
+    <meta name="description" content="A Tech solution driven, Android and Web
+    
+    Backend Development.
+    ">
+    <meta name="keywords"
+        content="Israel, Israel Akowe, easyakowe, easyrael, Alpher_i, alpher_i, Israel, Uma-ojo, coder, ML guy, israko.
+        Android Developer,Android developer, android developer, Web backend developer, android developer in Uyo, best android developer in Uyo,android developers in uyo, android developers in uyo, data scientist in Uyo, Machine Learning engineer, machine learning engineer in Uyo, machine learning engineers in Lagos">
+
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="7 days">
+    <meta name="author" content="Israel Akowe">
+
+
+
+<title>Israel CV Webpage</title>
+</head>
+
+<script type="text/javascript">
+	function validateform(){ 
+		var name = document.forms["myform"]["fullname"].value; 
+		var email = document.forms["myform"]["email"].value; 
+		var title = document.forms["myform"]["title"].value; 
+		var message = document.forms["myform"]["message"].value; 
+		if(name.length < 4){ 
+			alert("Name must be at least 4 characters long"); 
+			return false; 
+		} else if(message.length < 20){ 
+			alert("Message must be at least 20 characters long"); 
+			return false; 
+		} else { 
+		alert("Message sent successfully!"); 
+			return true;
+		} 
+		} 
+</script>
+
+<body style="font-family: Trebuchet MS;"> 	
+<div class="myname">		
+<table style="border: 1px solid #BB1924">			
+<tr style="background: #BB1924; width: 100%;">				
+<td style="color: #fff; padding-left: 8px; width: 80%;">					
+	<strong><h1 style="font-size: 45px;" class="name">Akowe Israel Uma-ojo</h1>
+	</strong><h5 style="font-weight: normal" class="intro">A highly spirited individual committed to maximum utilization of available resources in yielding quantifiable output and increasing value. I am confident, experienced, an astute learner, outspoken and a good team player all of which makes me a productive individual</h5>				
+</td>				
+<td style="width: 20%;">					
+	<img style="float: center; border-radius: 50%; margin-left: 65px;" src="https://res.cloudinary.com/easyakowe/image/upload/v1566490729/me.jpg" alt="Akowe Israel" width="140" height="140">				
+</td>
+</tr> 		
+</table>	
+</div>		
+<div>		
+<table style="padding-left: 12px;">			
+<tr style="font-size: 12px;">		
+<td>
+<h4 style="color: #BB1924";>Address:</h4>
+</td>				
+<td>13, Abraham Akinola Str., Onipetesi Estate, Mangoro, Lagos</td>		
+<td></td>				
+<td><h4 style="color: #BB1924">Phone:</h4></td>		
+<td>07063774241</td>		
+<td></td>				
+<td><h4 style="color: #BB1924">Email:</h4></td>		
+<td><a href="mailto:israelakowe@gmail.com" target="_blank">israelakowe@gmail.com</a></td>				
+<td></td>			
+<td></td>				
+<td><h4 style="color: #BB1924">To Contact Me, click </h4></td>		
+<td><a style="font-size: 18px;" href="#form_class">HERE</a></td>
+</tr>		
+</table>	
+</div> 
+
+<!-- USE TABLE ELEMENT FOR THE STRUCTURING --> 
+
+<div>		
+<table style="border: 2px solid #BB1924">		
+<tr>		
+<td style="border: 1px solid #BB1924; padding-left: 12px;">			<h2 style="color: #BB1924;" class="exp-head">Experience</h2>			<h4 class="exp-loc">McAdolph Institute of Information Technology, <span>Dalemo, 			Lagos-Abeokuta Expr. Way -- <em>SIWES Intern</em></span></h4>			<h5 style=" font-size: 12px;" class="exp-date">APRIL 2017 - NOVEMBER 2017</h5>			<h4 style="font-weight: normal; font-size: 12px;" class="exp-detail">I worked here as an intern during my Student Industrial Work Experience Scheme (SIWES) where I learnt Graphics Design and Web Development</h4>					</td>		<td style="border: 1px solid #BB1924; padding-left: 12px;">			<h2 style="color: #BB1924;" class="skls-head">Skills</h2>			<h4 style="font-weight: normal; font-size: 13px;" class="exp-detail">Graphics Design, Web design and development and Android Development</h4>			<h4 style="font-size: 14px;" class="list"> - HTML | CSS | PHP | SQL | XML | JAVA | PYTHON</h4>			<br><br><br>		</td>		</tr> 		<tr>		<td style="border: 1px solid #BB1924; padding-left: 12px;">			<h2 style="color: #BB1924" class="edu-head">Education</h2>			<strong><h4 class="edu-loc">Federal University Lokoja, Lokoja, Kogi State -- <em> B.Sc. Computer Science </em></h4></strong>			<h5 style="font-size: 12px;" class="edu-date">OCTOBER 2014 - SEPTEMBER 2018</h5>					</td>		<td style="border: 1px solid #BB1924; padding-left: 12px;">			<h2 style="color: #BB1924" class="awrd-head">Awards</h2>			<h4 style="font-weight: normal; font-size: 13px;">MTN Foundation Undergraduate Scholarship Award -<strong> 2015</strong></h4>			<h4 style="font-weight: normal; font-size: 13px;">Nigerian Universities Computer Programming Contest 2018 - <strong> 6th Place</strong></h4>					</td>		</tr> 		<tr>		<td style="border: 1px solid #BB1924; padding-left: 12px;">			<h2 style="color: #BB1924" class="pro-head">Projects</h2>			<div class="pro"><fieldset>				<strong><h4 class="pro-loc">Android Application for Small Holder Farmers in Nigeria</h4></strong>				<h4 style="font-weight: normal; font-size: 13px;" class="pro-detail">This project entails the development of an informative application for small-holder farmers in Nigeria on pressing agricultural issues like crop production, communication and security. I used Android Studio</h4>			</div><br> 			<div class="pro"><fieldset>				<strong><h4 class="pro-loc">Android Complaint Management App for Abuja Electricity Distribution Company				</h4></strong>				<h4 style="font-weight: normal; font-size: 13px;" class="pro-detail">This project entails the development of an complaint processing app for customers of the Abuja Electriity Distribution Company. I used Android Studio.</h4></fieldset>			</div><br> 			<div class="pro"><fieldset>				<strong><h4 class="pro-loc">A Simple Objective Quiz App</h4></strong>				<h4 style="font-weight: normal; font-size: 13px;" class="pro-detail">This light web app was developed as a practical work where a user can login or register, take a test and then have his or her result displayed to him or her instantly. I used HTML, CSS, PHP and SQL</h4></fieldset>			</div><br> 			<div class="pro"><fieldset>				<strong><h4 class="pro-loc">Staff Profile web Portal for Federal University Lokoja</h4></strong>				<h4 style="font-weight: normal; font-size: 13px;" class="pro-detail">I am currently working on a simple staff profile portal for my school for proper management and regulation of staff members in the institution. I am using HTML, CSS, JavaScript, PHP and SQL.</h4></fieldset>			</div></fieldset>		</td>		<td style="border: 1px solid #BB1924; padding-left: 12px;">			<div class="lang">				<h2 style="color: #BB1924" class="lang-head">Languages</h2>				<h4 style="font-weight: normal; font-size: 13px;">Fluency in English Language, Igbo, Yoruba and Pidgin</h4>			</div><br>			<div class="hbbs">				<h2 style="color: #BB1924" class="hbbs-head">Hobbies</h2>				<h4 style="font-weight: normal; font-size: 13px;">Learning new things, Reading, Gaming, Solving Problems and challenging individuals that they can achieve anything they put their mind to</h4>			</div><br>			<div>				<div>					<h3 style="color: #BB1924; font-size: 22px;"><b>Independent Courses</b></h3>					<ul> <li> <span id="course-name">HTML & CSS for Beginners – Web Fundamentals</span> – Codecademy.com</li><br> <li> <span id="course-name">Programming for Everybody in Python </span> - Coursera.org</li><br> <li> <span id="course-name">Introduction to Machine Learning </span> - Coursera.org</li><br> <li> <span id="course-name">Introduction to Python - The Basics</span> - Sololearn.com</li><br> <li> <span id="course-name">Java Programming Language</span> - Sololearn.com</li> </ul>					<br><br><br><br>				</div>							</div>		</td>				</tr>		
+
+
+<tr>		
+<td>			
+<div id="form_class" style="margin-left:16px">			
+
+<h3 style="color: #BB1924; font-size: 22px;"><b>Contact Me</b></h3>			
+
+<form name="myform" action="submit.php" method="POST" onsubmit="return validateform()"> 			
+
+<label class="input-title">Full Name</label><br>			
+<input class="input-cl" name="fullname" type="text" required=""><br> 			
+
+<label class="input-title">Email</label><br>			
+<input class="input-cl" name="email" type="email" required=""><br> 			
+
+<label class="input-title">Title</label><br>			
+<input class="input-cl" name="title" type="text" required=""><br> 			
+
+<label class="input-title">Message</label><br>			
+<textarea class="message-cl" name="message" type="text" required=""></textarea> 			
+
+<button class="btn-cl" name="submit" type="submit">Send</button>			
+</form>			
+</div>		
+</td>		
+</tr>
+
+</table>	
+</div> 
+
+
+
+
+<style type="text/css">
+.input-cl {
+height: 30px;
+width: 100%;
+border-radius: 10px;
+shadow: 1px;
+margin-bottom: 15px;
+}
+.message-cl {
+height: 70px;
+width: 100%;
+border-radius: 10px;
+shadow: 1px;
+margin-bottom: 15px;
+}
+.btn-cl {
+height: 50px;
+width: 30%;
+background: #BB1924;
+color: #ffffff;
+float: right;
+border-radius: 10px;
+shadow: 1px;
+margin-bottom: 15px;
+}
+</style>
+</body>
+</html>
